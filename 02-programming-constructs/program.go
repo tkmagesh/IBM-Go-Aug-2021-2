6e45af1f8fecab1c9c84526a771831c8ef82c49e
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-23 14:17:02 +0530

    switch construct

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -48,4 +48,48 @@ func main() {
 		}
 	}
 	fmt.Println(x)
+
+	//switch case construct
+	/*
+		no := 20
+		remainder := no % 2
+		switch remainder {
+		case 0:
+			fmt.Println(no, " is an even number")
+		case 1:
+			fmt.Println(no, " is an odd number")
+		}
+	*/
+
+	/*
+		switch no := 20; {
+		case no%2 == 0:
+			fmt.Println(no, " is an even number")
+		case no%2 == 1:
+			fmt.Println(no, " is an odd number")
+		}
+	*/
+
+	/*
+		rank by score
+		score 0 - 3 -> "Terrible"
+		score 4 - 7 -> "Not Bad"
+		score 8 - 9 -> "Good"
+		score 10 -> "Excellent"
+		otherwise -> "Unknown score"
+	*/
+
+	score := 6
+	switch score {
+	case 0, 1, 2, 3:
+		fmt.Println("Terrible")
+	case 4, 5, 6, 7:
+		fmt.Println("Not Bad")
+	case 8, 9:
+		fmt.Println("Good")
+	case 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("Unknown score")
+	}
 }
