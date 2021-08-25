d6ed916cc9bef3057b93b6d0e366f20fe5639bc6
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-25 15:04:21 +0530

    Update assignment-2.go

--- 09-interfaces/assignment-2.go
+++ 09-interfaces/assignment-2.go
@@ -1,21 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println(sum(10)) //=> 10
-	fmt.Println(sum(10, 20)) //=> 30
-	fmt.Println(sum(10, 20, 30, 40, 50)) //=> 150
-	fmt.Println(sum(10, 20, "30", 40, 50)) //=> 150
-	fmt.Println(sum(10, 20, "abc", 40, 50)) //=> 120
-	fmt.Println(sum(10, 20, "abc", []int{40, 50})) //=> 120
-	fmt.Println(sum(10, 20, "abc", []interface{40, "50"})) //=> 120
+	fmt.Println(sum(10))                                     //=> 10
+	fmt.Println(sum(10, 20))                                 //=> 30
+	fmt.Println(sum(10, 20, 30, 40, 50))                     //=> 150
+	fmt.Println(sum(10, 20, "30", 40, 50))                   //=> 150
+	fmt.Println(sum(10, 20, "abc", 40, 50))                  //=> 120
+	fmt.Println(sum(10, 20, "abc", []int{40, 50}))           //=> 120
+	fmt.Println(sum(10, 20, "abc", []interface{}{40, "50"})) //=> 120
 }
 
-func sum(nos ...int /* nos = slice of int */) int {
+func sum(nos ...interface{}) int {
 	result := 0
-	for i := 0; i < len(nos); i++ {
-		result += nos[i]
+	for _, no := range nos {
+		switch v := no.(type) {
+		case int:
+			result += v
+		case string:
+			if val, err := strconv.Atoi(v); err == nil {
+				result += val
+			}
+		case []int:
+			intfList := make([]interface{}, len(v))
+			for idx, x := range v {
+				intfList[idx] = x
+			}
+			result += sum(intfList...)
+		case []interface{}:
+			result += sum(v...)
+		}
+
 	}
 	return result
 }
