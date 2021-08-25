a8ae21dc54000c51a949c6842cc58dfb60cadb8f
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-25 14:18:58 +0530

    using interface{}

---
+++ 09-interfaces/program-2.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+type Product struct {
+}
+
+type Dummy struct {
+}
+
+func (d *Dummy) M() {
+
+}
+
+type I interface {
+	M()
+}
+
+func main() {
+	var x interface{}
+	x = 100
+	x = "This is a string"
+	x = true
+	x = struct{}{}
+	x = []int{1, 2, 3}
+	fmt.Println(x)
+
+	var val interface{}
+	val = 100
+	if no, ok := val.(int); ok {
+		y := no + 200
+		fmt.Println(y)
+	} else {
+		fmt.Println("Not an int")
+	}
+
+	//val = "This is a string"
+	//val = struct{}{}
+	//val = Product{}
+	val = Dummy{}
+	switch v := val.(type) {
+	case int:
+		fmt.Printf("Double of val is %d\n", v*2)
+	case string:
+		fmt.Printf("Length of string %q is %d\n", v, len(v))
+	case []int:
+		fmt.Println("List of numbers => ", v)
+	case struct{}:
+		fmt.Println("Empty struct")
+	case Product:
+		fmt.Println("Instance of Product => ", v)
+	/* case I:
+	fmt.Println("implementation of I => ", v) */
+	default:
+		fmt.Println("Unknown type")
+	}
+
+}
