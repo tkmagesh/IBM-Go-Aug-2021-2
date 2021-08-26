8dc8724f8bc38683820620b1af121c3c3578255a
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-26 10:25:55 +0530

    divide & conquer assignment

---
+++ 11-concurrency/demo-10.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
+	fmt.Println(sum(data...))
+}
+
+func sum(nos ...int) int {
+	result := 0
+	for _, no := range nos {
+		result += no
+	}
+	return result
+}
+
+/*
+break the data in two sets
+aggregate them using 2 goroutines
+get the aggreagated result and calculate the final sum in the 'main' function and print it
+*/
