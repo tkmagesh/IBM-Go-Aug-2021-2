700e84c54cb00ccf768556dc46e44a7dbb13dd9b
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-26 10:51:35 +0530

    divide & conquer assignment solution

--- 11-concurrency/demo-10.go
+++ 11-concurrency/demo-10.go
@@ -4,15 +4,21 @@ import "fmt"
 
 func main() {
 	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
-	fmt.Println(sum(data...))
+	dataSet1 := data[:len(data)/2]
+	dataSet2 := data[len(data)/2:]
+	resultCh := make(chan int)
+	go sum(resultCh, dataSet1...)
+	go sum(resultCh, dataSet2...)
+	finalResult := <-resultCh + <-resultCh
+	fmt.Println(finalResult)
 }
 
-func sum(nos ...int) int {
+func sum(resultCh chan int, nos ...int) {
 	result := 0
 	for _, no := range nos {
 		result += no
 	}
-	return result
+	resultCh <- result
 }
 
 /*
