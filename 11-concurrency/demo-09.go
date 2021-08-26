16a0ef445a6874c7ceaf1a6f69ee14f6c41a7cb4
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-26 11:08:11 +0530

    Update demo-09.go

--- 11-concurrency/demo-09.go
+++ 11-concurrency/demo-09.go
@@ -8,12 +8,11 @@ import (
 func main() {
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+	ch1 := make(chan int, 1)
+	ch2 := make(chan int, 1)
 	go print("Hello", time.Second*2, ch1, ch2, wg)
 	go print("World", time.Second*1, ch2, ch1, wg)
 	ch1 <- 1
-	//TO BE FIXED
 	wg.Wait()
 }
 
